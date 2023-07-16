44bc6e9c9006ad897ed6a540ab71b196fb81448f
Thomas Sounack <tsounack@stanford.edu>
2023-07-16 18:50:44 +0200

    parallel update

--- vilmedic/blocks/classifier/evaluation.py
+++ vilmedic/blocks/classifier/evaluation.py
@@ -24,7 +24,7 @@ def evaluation(models, config, dl, **kwargs):
             num_samples = len(dl.dataset)
             logits_list = [np.zeros((num_samples, len(models), num_classes)) for _ in range(batch_size)]
             labels_list = [np.zeros((num_samples, num_classes)) for _ in range(batch_size)]
-            losses_list = [[] for _ in range(len(dl))]
+            losses_list = []
 
         # iterating over the batch, stacking refs and hyps
         for i in range(batch_size):
@@ -35,15 +35,15 @@ def evaluation(models, config, dl, **kwargs):
         # Loss
         for j, r in enumerate(results):
             loss_value = r['loss'].cpu().item() if isinstance(r['loss'], torch.Tensor) and r['loss'].numel() == 1 else r['loss'].cpu().numpy()
-            losses_list[num_batch].append(loss_value)
+            losses_list.append(loss_value)
 
         cumulative_index += batch_size
 
-        break
+        # break
 
     logits = np.concatenate(logits_list, axis=0)
     labels = np.concatenate(labels_list, axis=0)
-    losses = np.concatenate(losses_list, axis=0)
+    losses = np.array(losses_list)
 
     preds = np.mean(logits, axis=1)
     loss = np.mean(losses)
