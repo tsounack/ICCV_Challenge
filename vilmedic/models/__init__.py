db9cce2929d8a773243d91d97eec52c5a11b868b
Thomas Sounack <tsounack@stanford.edu>
2023-07-16 18:09:53 +0200

    added mvqa3

--- vilmedic/models/__init__.py
+++ vilmedic/models/__init__.py
@@ -1,6 +1,7 @@
 from vilmedic.models.iccv.ICCV import ICCV
 from vilmedic.models.mvqa.MVQA import MVQA
 from vilmedic.models.mvqa2.MVQA2 import MVQA2
+from vilmedic.models.mvqa3.MVQA3 import MVQA3
 
 from vilmedic.models.rrs.SumRNN import SumRNN
 from vilmedic.models.rrs.SumHugMulti import SumHugMulti
